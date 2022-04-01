b72cad4480eb57bd358dca883748549b809ce40d
Mercy <mercycherotich757@gmail.com>
2022-04-01 11:30:30 +0300

    create Image model

--- core/django/instagram-clone/insta/models.py
+++ core/django/instagram-clone/insta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    caption = models.CharField(max_length =30)
+    
