95fc5a967cfaab22ae306678d63181cd210ac8d2
Mercy <mercycherotich757@gmail.com>
2022-04-01 15:16:59 +0300

    display images

--- core/django/instagram-clone/insta/urls.py
+++ core/django/instagram-clone/insta/urls.py
@@ -1,7 +1,16 @@
 from . import views
+from django.conf import settings
 from django.urls import re_path
+from django.conf.urls.static import static
+
 
 
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
+    re_path(r'^image/',views.display_image,name='displayImages'),
+    re_path(r'^search/', views.search_results, name='search_results'),
+
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
