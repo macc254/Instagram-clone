0518570ae653c2dc93ee411b187503b1b53a105a
Mercy <mercycherotich757@gmail.com>
2022-04-01 15:04:07 +0300

    create a view function to display the images

--- core/django/instagram-clone/insta/views.py
+++ core/django/instagram-clone/insta/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image,Profile
 
 
 # Create your views here.
 def welcome(request):
     return render(request, 'home.html')
+
+def display_image(request):
+    profile = Profile.objects.all()
+    image = Image.objects.all()
+    return render(request,'all-images.html',{'image':image,'profile':profile})
+
