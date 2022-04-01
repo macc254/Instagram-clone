3f2d6ca392a2a7de6baf7e76442adb48f7c645f8
Mercy <mercycherotich757@gmail.com>
2022-04-01 11:42:55 +0300

    create Profile model

--- core/django/instagram-clone/insta/models.py
+++ core/django/instagram-clone/insta/models.py
@@ -5,4 +5,16 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length =30)
     caption = models.CharField(max_length =30)
+    profile = models.ForeignKey( 'Profile', on_delete=models.CASCADE,default=1)
+
+
     
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+class Profile(models.Model):
+    bio = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.bio
