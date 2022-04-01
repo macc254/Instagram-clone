b3771fb677514aad0bc2df0c9d275395a930967c
Mercy <mercycherotich757@gmail.com>
2022-04-01 17:33:49 +0300

    setup authentication service

--- core/django/instagram-clone/insta/urls.py
+++ core/django/instagram-clone/insta/urls.py
@@ -1,7 +1,10 @@
-from . import views
 from django.conf import settings
-from django.urls import re_path
 from django.conf.urls.static import static
+from django.urls import re_path,include
+from django.conf import settings
+from django.contrib.auth import views 
+from . import views
+from django_registration.backends.one_step.views import RegistrationView
 
 
 
@@ -9,6 +12,12 @@ urlpatterns=[
     re_path('^$',views.profile,name = 'profile'),
     re_path(r'^image/',views.display_image,name='displayImages'),
     re_path(r'^search/', views.search_results, name='search_results'),
+    re_path('accounts/', include('django_registration.backends.one_step.urls')),
+    re_path('accounts/register/',
+        RegistrationView.as_view(success_url='/profile/'),
+        name='django_registration_register'),
+    # re_path(r'^logout/$', views.logout, {"next_page": '/'}), 
+    re_path(r'^tinymce/', include('tinymce.urls')),
 
 
 ]
