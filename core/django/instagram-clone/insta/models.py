5837184ef453bae8be227d29f601c6f0726ba74e
Mercy <mercycherotich757@gmail.com>
2022-04-01 14:48:21 +0300

    add migrations

--- core/django/instagram-clone/insta/models.py
+++ core/django/instagram-clone/insta/models.py
@@ -1,20 +1,24 @@
 from django.db import models
-
-# Create your models here.
-
 class Image(models.Model):
+    image = models.ImageField(upload_to='images/',default='1')
     name = models.CharField(max_length =30)
     caption = models.CharField(max_length =30)
-    profile = models.ForeignKey( 'Profile', on_delete=models.CASCADE,default=1)
-
-
+    profile = models.ForeignKey('Profile',on_delete=models.CASCADE,default='0')
+    likes = models.IntegerField()
+    comments = models.CharField(max_length =30)
     
     def __str__(self):
         return self.name
     class Meta:
         ordering = ['name']
+        
+    def save_image(self):
+        self.save()
+        
+        
 class Profile(models.Model):
     bio = models.CharField(max_length =30)
+    profile_photo = models.ImageField(upload_to ='images/',default='1')
 
     def __str__(self):
         return self.bio
--- core/django/instagram-clone/insta/tests.py
+++ core/django/instagram-clone/insta/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Image, Profile
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.roku= Image(name = 'roku', caption ='Fire-Lord')
+    #Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.roku,Image))
+     # Testing Save Method
+    def test_save_method(self):
+        self.roku.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
